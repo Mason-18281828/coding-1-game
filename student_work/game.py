46330bc49c52f61ecefc9e257d8a1c4a23b89b92
Mason-18281828 <s1093452@student.knoxschools.org>
2026-02-19 12:18:48 -0500

    I added a bunch of ideas for the game in both files.

--- student_work/game.py
+++ student_work/game.py
@@ -1,7 +1,41 @@
 # Write your game here
 
 game_data = {
-    # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
+    'portal_spawn': False,
+    'rooms': {
+        # rooms order: zero closed, one closed, two closed, three closed
+        # closed order: top, left, right, bottom
+    },
+    'starter_room': game_data['rooms'][0],
+    'enemies': {
+        # whatever we come up with
+    },
+    'gems': {
+        # daimonds, rubies, etc.
+    }
+    'boss': {
+        # boss stats
+    }
+    'room_details': {
+        'plants': {
+            'vines': [],
+            'bushes': [],
+            'grass_patches': []
+        },
+        'chests': [],
+        'dirt_floor': []
+    }
+}
+
+player_data = {
+    'user_name': '',
+    'gem_num': 0,
+    'health': 5,
+    'speed': 1,
+    'models': {
+        # Whatever models we design. Can be just one.
+    },
+    'monster_repellent': 0
 }
 
 def draw_board(screen):

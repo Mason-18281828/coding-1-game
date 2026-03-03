4abb8943ed5c3de9aa1298045ee82c289ada8562
Mason-18281828 <s1093452@student.knoxschools.org>
2026-03-03 17:18:06 +0000

    walls and collecting stuff

--- student_work/game.py
+++ student_work/game.py
@@ -6,6 +6,7 @@
 
 # To make this work, you may have to type this into the terminal --> pip install curses
 import curses
+import random
 
 game_data = {
     'width': 6,
@@ -13,11 +14,11 @@ game_data = {
     'player': {"x": 3, "y": 3, "score": 0},
     # 'eagle_pos': {"x": 4, "y": 4},
     'collectibles': [
-        {"x": 3, "y": 1, "collected": False},
+        {"x": 3, "y": 1, "collected": False, "how many": 0},
     ],
     'hearts': ['live', 'live', 'live', 'live', 'live'],
-    'rooms': {
-        'walls_1': [
+    'doorways + basics': {
+        'basics': [
             {"x": 1-1, "y": 1-1},
             {"x": 1-1, "y": 2-1},
             {"x": 1-1, "y": 5-1},
@@ -33,8 +34,49 @@ game_data = {
             {"x": 6-1, "y": 1-1},
             {"x": 6-1, "y": 2-1},
             {"x": 6-1, "y": 5-1},
-            {"x": 6-1, "y": 6-1},
+            {"x": 6-1, "y": 6-1}
+        ],
+        'block top': [
+            {"x": 3-1, "y": 1-1},
+            {"x": 4-1, "y": 1-1},
+            {"x": 3-1, "y": 2-1},
+            {"x": 4-1, "y": 2-1}
+        ],
+        'block left': [
+            {"x": 1-1, "y": 3-1},
+            {"x": 2-1, "y": 3-1},
+            {"x": 1-1, "y": 4-1},
+            {"x": 2-1, "y": 4-1}
+        ],
+        'block right': [
+            {"x": 5-1, "y": 3-1},
+            {"x": 6-1, "y": 3-1},
+            {"x": 5-1, "y": 4-1},
+            {"x": 6-1, "y": 4-1}
+        ],
+        'block down': [
+            {"x": 3-1, "y": 5-1},
+            {"x": 4-1, "y": 5-1},
+            {"x": 3-1, "y": 6-1},
+            {"x": 4-1, "y": 6-1}
         ]
+    },
+    'rooms': {
+        'walls_1': [],
+        'walls_2': [],
+        'walls_3': [],
+        'walls_4': [],
+        'walls_5': [],
+        'walls_6': [],
+        'walls_7': [],
+        'walls_8': [],
+        'walls_9': [],
+        'walls_10': [],
+        'walls_11': [],
+        'walls_12': [],
+        'walls_13': [],
+        'walls_14': [],
+        'walls_15': []
     },
 
     # ASCII icons
@@ -47,13 +89,29 @@ game_data = {
     'empty': "  "
 }
 
+game_data["rooms"]["walls_1"] = [a for a in game_data['doorways + basics']['basics']]
+game_data["rooms"]["walls_2"] = [a for a in game_data['doorways + basics']['basics'] + game_data['doorways + basics']['block top']]
+game_data["rooms"]["walls_3"] = [a for a in game_data['doorways + basics']['basics'] + game_data['doorways + basics']['block left']]
+game_data["rooms"]["walls_4"] = [a for a in game_data['doorways + basics']['basics'] + game_data['doorways + basics']['block right']]
+game_data["rooms"]["walls_5"] = [a for a in game_data['doorways + basics']['basics'] + game_data['doorways + basics']['block down']]
+game_data["rooms"]["walls_6"] = [a for a in game_data['doorways + basics']['basics'] + game_data['doorways + basics']['block top'] + game_data['doorways + basics']['block left']]
+game_data["rooms"]["walls_7"] = [a for a in game_data['doorways + basics']['basics'] + game_data['doorways + basics']['block top'] + game_data['doorways + basics']['block right']]
+game_data["rooms"]["walls_8"] = [a for a in game_data['doorways + basics']['basics'] + game_data['doorways + basics']['block top'] + game_data['doorways + basics']['block down']]
+game_data["rooms"]["walls_9"] = [a for a in game_data['doorways + basics']['basics'] + game_data['doorways + basics']['block left'] + game_data['doorways + basics']['block right']]
+game_data["rooms"]["walls_10"] = [a for a in game_data['doorways + basics']['basics'] + game_data['doorways + basics']['block left'] + game_data['doorways + basics']['block down']]
+game_data["rooms"]["walls_11"] = [a for a in game_data['doorways + basics']['basics'] + game_data['doorways + basics']['block right'] + game_data['doorways + basics']['block down']]
+game_data["rooms"]["walls_12"] = [a for a in game_data['doorways + basics']['basics'] + game_data['doorways + basics']['block top'] + game_data['doorways + basics']['block left'] + game_data['doorways + basics']['block right']]
+game_data["rooms"]["walls_13"] = [a for a in game_data['doorways + basics']['basics'] + game_data['doorways + basics']['block top'] + game_data['doorways + basics']['block left'] + game_data['doorways + basics']['block down']]
+game_data["rooms"]["walls_14"] = [a for a in game_data['doorways + basics']['basics'] + game_data['doorways + basics']['block top'] + game_data['doorways + basics']['block right'] + game_data['doorways + basics']['block down']]
+game_data["rooms"]["walls_15"] = [a for a in game_data['doorways + basics']['basics'] + game_data['doorways + basics']['block left'] + game_data['doorways + basics']['block right'] + game_data['doorways + basics']['block down']]
+
 def draw_board(stdscr):
     curses.start_color()
     curses.use_default_colors()
     curses.init_pair(1, curses.COLOR_WHITE, -1)
 
     stdscr.clear()
-    room = 1
+    room = random.randint(1, 9)
     for y in range(game_data['height']):
         row = ""
         for x in range(game_data['width']):
@@ -79,7 +137,14 @@ def draw_board(stdscr):
         stdscr.addstr(y, 0, row, curses.color_pair(1))
 
     
+def check_collectibles():
+    for c in game_data['collectibles']:
+        if (not c["collected"] and
+            game_data['player']["x"] == c["x"] and
+            game_data['player']["y"] == c["y"]):
 
+            c["collected"] = True
+            game_data['collectibles']['how many'] += 1
 
 def move_player(key):
     x = game_data['player']['x']
@@ -123,6 +188,8 @@ def main(stdscr):
         if key:
             if key.lower() == "q":
                 break
+
+            # put collectables here?
 
             move_player(key)
             draw_board(stdscr)
